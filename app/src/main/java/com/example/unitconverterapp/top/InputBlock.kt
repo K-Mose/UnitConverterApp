bdaacb208075f06027fbda290fcc4bf01a2a6dcf
Mose <km_413@naver.com>
2022-06-21 15:41:55 +0900

    Top Screen - Convert Button

--- app/src/main/java/com/example/unitconverterapp/top/InputBlock.kt
+++ app/src/main/java/com/example/unitconverterapp/top/InputBlock.kt
@@ -1,17 +1,18 @@
 package com.example.unitconverterapp.top
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +25,9 @@ import com.example.unitconverterapp.ui.theme.UnitConverterAppTheme
 fun InputBlock(
     conversion: Conversion,
     inputText: MutableState<String>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    context: Context = LocalContext.current,
+    calculate: (String) -> Unit
 ) {
     Column(
         modifier = Modifier.padding(0.dp, 20.dp, 0.dp, 0.dp)
@@ -50,6 +53,25 @@ fun InputBlock(
                 modifier = modifier
                     .padding(10.dp, 30.dp, 0.dp, 0.dp)
                     .fillMaxWidth(0.35F)
+            )
+        }
+
+        Spacer(modifier = modifier.height(20.dp))
+
+        OutlinedButton(
+            onClick = {
+                if (inputText.value != "")
+                    calculate(inputText.value)
+                else
+                    Toast.makeText(context, "Please, enter your value", Toast.LENGTH_LONG).show()
+            },
+            modifier = modifier.fillMaxWidth(1F)
+        ) {
+            Text(
+                text = "Convert",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
             )
         }
     }
